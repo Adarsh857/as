101138dae705373bb30f36c303074b3d9c7a0a34
adarsh sharma <sharma2015adarsh@gmail.com>
2022-02-12 23:56:34 +0530

    blog

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,12 @@
 import { motion } from 'framer-motion'
-import { lazy, Suspense } from 'core-js/library/fn/reflect/es7/metadata'
+import { lazy, Suspense } from 'react'
 import styled, { keyframes, ThemeProvider } from 'styled-components'
 import { DarkTheme, mediaQueries } from './Themes'
 import astronaut from "../assets/Images/spaceman.png";
-import Loding from '../subComponents/Loading';
+import Loading from '../subComponents/Loading';
 
-///////components
+//components
+
 const SocialIcons = lazy(() =>
     import ('../subComponents/SocialIcons'))
 const PowerButton = lazy(() =>
@@ -13,38 +14,43 @@ const PowerButton = lazy(() =>
 const LogoComponent = lazy(() =>
     import ('../subComponents/LogoComponent'))
 const ParticlesComponent = lazy(() =>
+    import ('../subComponents/ParticlesComponent'))
+const BigTitle = lazy(() =>
     import ('../subComponents/BigTitle'))
 
 const Box = styled(motion.div)
 `
-background-color: ${(props)=>props.theme.body};
-width: 100vw;
-height:100vh;
-positon: relative;
-overflow:hidden;
-`
+	background-color: ${(props) => props.theme.body};
+  	width: 100vw;
+  	height: 100vh;
+  	position: relative;
+  	overflow: hidden;
+	`
+
 const float = keyframes `
-0% {transform: translateY(-10px)  }
-50% {transform: translate(15px) translateX(15px)  }
-100%{transform: translateY(-10px)   }
-`
+	0% { transform: translateY(-10px)         }
+	50% { transform: translateY(15px) translateX(15px)        }
+	100% { transform: translateY(-10px)         }
+	`
+
 const SpaceMan = styled(motion.div)
 `
-position:absolute
-top: 10%;
+	position: absolute;
+  	top: 10%;
   	right: 5%;
   	animation: ${float} 4s ease infinite;
 	width:20vw;
 	img{
 		width:100%;
 		height:auto;
-  	}`
+  	}
+	`
+
 const Main = styled(motion.div)
 `
-
-      border: 2px solid ${(pprops) => props.theme.text};
-      color: ${(props)=> props.theme.text};
-      padding: 2rem;
+	border: 2px solid ${(props) => props.theme.text};
+  	color: ${(props) => props.theme.text};
+  	padding: 2rem;
   	width: 50vw;
   	height: 60vh;
   	z-index: 3;
@@ -81,20 +87,22 @@ const Main = styled(motion.div)
 	`};
 
 `
-AboutPage = ()=>{
-    turn(
-        <ThemeProvider theme={DarkTheme}>
-            <Suspense fallback={<Loading/>}>
-                <Box
-                key='skills'
-                initial={{opacity:0}}
-                animate={{opacity: 1,transaction: {duration:0.5}}}
-                exit={{opacity:0,transaction:{duration:0.5}}}>
-                    <LogoComponent theme ='dark'/>
-                    <PowerButton/>
-                    <SocialIcons theme ='dark'/>
-                    <ParticlesComponent theme ='dark'/>
-                    <SpaceMan
+const AboutPage = () => {
+	return (
+		<ThemeProvider theme={DarkTheme}>
+	  		<Suspense fallback={<Loading/>}>
+				<Box
+		  			key='skills'
+		  			initial={{ opacity: 0 }}
+		  			animate={{ opacity: 1, transition: { duration: 0.5 } }}
+		  			exit={{ opacity: 0, transition: { duration: 0.5 } }}>
+		  			
+					<LogoComponent theme='dark' />
+		  			<PowerButton />
+		  			<SocialIcons theme='dark' />
+		  			<ParticlesComponent theme='dark' />
+
+		  			<SpaceMan
 							initial={{ right: '-20%', top: '100%' }}
 							animate={{ right: '5%', top: '10%', transition: { duration: 2, delay: 0.5 },
 					}}>
@@ -106,9 +114,9 @@ AboutPage = ()=>{
 						animate= {{opacity: 1, transition: {duration: 1, delay: 1}}}>
 							Currently pursuing Bachelors of Technology from Chandigarh
 							Engineering College, CGC Landran.
-							Skilled in C, C++, Web Development(HTML, CSS, Java Script and reactJS)
+							Skilled in C, C++, Web Development(HTML,CSS,Java Script,ReactJS,Redux,Native)
 							with Good Problem Solving Skills and good Knowledge of Data Structure.
-							5 star C and C++ badge on Hackerrank.
+							6 star Coder on Hackerrank.
 					</Main>
 					<BigTitle text='ABOUT' top='10%' left='5%' />
 				</Box>
